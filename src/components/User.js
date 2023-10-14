904ff0262ff1783a17db634c416c9f315e7ffc74
Prachi Shende <prachi.shende1997@gmail.com>
2023-10-14 12:44:01 +0530

    how to update state in class components

--- src/components/User.js
+++ src/components/User.js
@@ -4,14 +4,16 @@ import { useState } from 'react';
 const User = (props) => {
 
     const[count,setCount]=useState(0);
+    const[count2,setCount2]=useState(1);
 
   return (
     <div className='user-card'>
         <h2>Name : {props.name}</h2>
         <h3>Location : Nagpur</h3>
         <h4>Contact : p@gmail.com</h4>
-        <h1>{count}</h1>
+        <h1>count : {count}</h1>
         <button onClick={()=>setCount(count+1)}>update count</button>
+        <h1>count : {count2}</h1>
     </div>
   )
 }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,14 +6,32 @@ class UserClass extends React.Component{
 constructor(props){
     super(props);
     console.log(props)
+
+    //state  in class component
+    this.state={
+      count:0,
+      count2:1,
+    };
+
 }
 
   render(){
 
     const{name,location}=this.props;
+    const{count,count2}=this.state
 
     return(
         <div className='user-card'>
+          <h1>Count : {this.state.count}</h1>
+          <h1>Count : {count}</h1>
+          <h1>Count : {count2}</h1>
+          <button onClick={()=>{
+            this.setState({
+              count:this.state.count+1,
+              count2:this.state.count2+1,
+            });
+          }}>Count Increase</button>
+
         <h2>Name : {name}</h2>
         <h3>Location : {location}</h3>
         <h4>Contact : p@gmail.com</h4>
