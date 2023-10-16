e46b3174a6e7dbcded34ce9308b98539d6cf8e71
Prachi Shende <prachi.shende1997@gmail.com>
2023-10-16 17:49:13 +0530

    Lazy loading = episode09

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,6 +23,7 @@ const Header=()=>{
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About Us</Link></li>
                 <li><Link to="/contact">Contact Us</Link></li>
+                <li><Link to="/Grocery">Grocery</Link></li>
                 <li><Link to="/cart">Cart</Link></li>
                 <button className="login" 
                 onClick={()=>{
