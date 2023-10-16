fb655986db31768a28ea076caa089a8d0ac53912
Prachi Shende <prachi.shende1997@gmail.com>
2023-10-16 14:11:21 +0530

    Custom Hooks In react

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,49 +2,87 @@ import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import { MENU_API } from "../utils/constants";
-
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
 
-const [resInfo, setResInfo] = useState(null);
+   const { resId }= useParams();
 
-const { resId }= useParams();
-
- useEffect(()=>{
-    fetchMenu();
- },[]);
-
- const fetchMenu = async ()=>{
-    const data = await fetch (MENU_API + resId + "&catalog_qa=undefined&submitAction=ENTER");
+   const resInfo = useRestaurantMenu(resId);
+   //for this particular resId restaurant menu data should come while we fetching data using useRestaurantMenu Hook
+   
+   
+   
+   if(resInfo === null) return <Shimmer/>;
+   
+   const {name,costForTwoMessage,cuisines} =resInfo?.cards[0]?.card?.card?.info ;
     
-    const json = await data.json();
-    console.log(json);
+   const{itemCards}= resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+   console.log(itemCards)
+   
+    return(
+       <div>
+           <h1>Restaurants Menu</h1>
+           <h2>{name}</h2>
+           <p>{cuisines.join(", ")}  - {costForTwoMessage} </p>
+           
+           <h2>menu</h2>
+           <ul>
+            {itemCards.map(item => 
+            <li key={item.card.info.id}>
+               {item.card.info.name} - {"Rs."} {item.card.info.price/100 || item.card.info.defaultPrice/100 }
+            </li>)}
+               {/* <li>{itemCards[0].card.info.name}</li> */}
+           </ul>
+       </div>
+      )
+   }
+   export default RestaurantMenu;
 
-    setResInfo(json.data) 
- }
 
-if(resInfo === null) return <Shimmer/>;
 
-const {name,costForTwoMessage,cuisines} =resInfo?.cards[0]?.card?.card?.info ;
+
+// const RestaurantMenu = () => {
+
+// const [resInfo, setResInfo] = useState(null);
+
+// const { resId }= useParams();
+
+//  useEffect(()=>{
+//     fetchMenu();
+//  },[]);
+
+//  const fetchMenu = async ()=>{
+//     const data = await fetch (MENU_API + resId + "&catalog_qa=undefined&submitAction=ENTER");
+    
+//     const json = await data.json();
+//     console.log(json);
+
+//     setResInfo(json.data) 
+//  }
+
+// if(resInfo === null) return <Shimmer/>;
+
+// const {name,costForTwoMessage,cuisines} =resInfo?.cards[0]?.card?.card?.info ;
  
-const{itemCards}= resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
-console.log(itemCards)
+// const{itemCards}= resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+// console.log(itemCards)
 
- return(
-    <div>
-        <h1>Restaurants Menu</h1>
-        <h2>{name}</h2>
-        <p>{cuisines.join(", ")}  - {costForTwoMessage} </p>
+//  return(
+//     <div>
+//         <h1>Restaurants Menu</h1>
+//         <h2>{name}</h2>
+//         <p>{cuisines.join(", ")}  - {costForTwoMessage} </p>
         
-        <h2>menu</h2>
-        <ul>
-         {itemCards.map(item => 
-         <li key={item.card.info.id}>
-            {item.card.info.name} - {"Rs."} {item.card.info.price/100 || item.card.info.defaultPrice/100 }
-         </li>)}
-            {/* <li>{itemCards[0].card.info.name}</li> */}
-        </ul>
-    </div>
-   )
-}
-export default RestaurantMenu;
+//         <h2>menu</h2>
+//         <ul>
+//          {itemCards.map(item => 
+//          <li key={item.card.info.id}>
+//             {item.card.info.name} - {"Rs."} {item.card.info.price/100 || item.card.info.defaultPrice/100 }
+//          </li>)}
+//             {/* <li>{itemCards[0].card.info.name}</li> */}
+//         </ul>
+//     </div>
+//    )
+// }
+// export default RestaurantMenu;
